e719ed3af1c95523ac2475475548ced50eee7665
SGrace11 <sgrace8@uncc.edu>
2022-03-15 19:17:11 -0400

    Working on e2e testing of search_movies

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,1 +1,12 @@
 # TODO: Feature 3
+from flask.testing import FlaskClient
+from app import app
+
+def test_search_movies_page(test_app: FlaskClient):
+    response = test_app.get('/movies/search/?movie-title=Good-Test')
+    response_data = response.data;
+    assert 7 in response_data;
+
+    response = test_app.get('/movies/search/?movie-title=Bad-Test')
+    response_data = response.data;
+    assert "No such movie in database" in response_data;
--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,5 +1,4 @@
 # TODO: Feature 3
-import pytest
 from src.repositories.movie_repository import movie_repository_singleton
 
 def search(title):
@@ -10,8 +9,8 @@ def search(title):
         return -1;
 
 def test_get_movie_by_title():
-    movie_repository_singleton.create_movie('Good Test', 'Hi', 7);
+    movie_repository_singleton.create_movie('Good-Test', 'Hi', 7);
     movie_repository_singleton.create_movie('Another Test', 'Hello', 2);
 
-    assert search('Good Test') == 7;
+    assert search('Good-Test') == 7;
     assert search('Bad Test') == -1;
